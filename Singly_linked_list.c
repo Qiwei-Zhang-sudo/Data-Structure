599296c535b0756de483520d661c09572d201f3f
zqw--sudu <2197134710@qq.com>
2026-01-03 17:39:25 +0800

    Singly-link-list-with-a-head-node

--- Singly_linked_list.c
+++ Singly_linked_list.c
@@ -18,7 +18,34 @@ bool Initlist(Linklist *l)
 }
 
 int main(){
-    Linklist l;
-    Initlist(&l);
+    Linklist head,p,d;
+    if(!Initlist(&head))
+        printf("链表初始化失败！\n");
+    p = head;
+    printf("请输入0以外的数据，继续循环\n");
+    do{
+        d = (LNode *)malloc(sizeof(LNode));
+        if(d==NULL)
+            {
+                printf("新节点分配失败\n");
+                return -1;
+            }
+        printf("Please add your data:");
+        scanf("%d", &d->data);
+        if (d->data == 0)
+        {
+            free(d);
+            break;
+        }
+        d->next = NULL;
+        p->next = d;
+        d = d;
+    } while (1);
+    p = head;
+    while(p!=NULL){
+        LNode *temp = p;
+        p = p->next;
+        free(temp);
+    }
     return 0;
 }
