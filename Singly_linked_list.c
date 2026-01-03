eec1bb5138662b8c1109c7343e8f2469cc1dc6a5
zqw--sudu <2197134710@qq.com>
2026-01-03 15:25:07 +0800

    Initlist

--- Singly_linked_list.c
+++ Singly_linked_list.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-typedef
+typedef struct LNode{
+    int data;
+    struct LNode *next;
+} LNode ,*Linklist;
+
+bool Initlist(Linklist *l)
+{
+    *l = (LNode *)malloc(sizeof(LNode));
+    if(*l==NULL){
+        return false;
+    }
+    (*l)->next = NULL;
+    return true;
+}
 
 int main(){
-
+    Linklist l;
+    Initlist(&l);
+    return 0;
 }
